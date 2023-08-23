7811fc165a3f08e99da3d5cef8bb2c699a850866
Apple Juice <mallari23adrian@gmail.com>
2023-08-23 11:44:05 +0800

    added routing to preview after snapsnot

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -1,21 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Stack, router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import axios from "axios";
 
 const Home = () => {
-	return (
-		<>
-			<View>
-				<Stack.Screen options={{ title: "Home" }} />
-				<View>
-					<Text className="text-[99px]">Hello World</Text>
-					<Text>This is the first page of your app.</Text>
-					<Link href="/TakeAPicture">Take a Picture</Link>
-					<Link href="/Upload">Upload</Link>
-				</View>
-			</View>
-		</>
-	);
+  const [weatherData, setWeatherData] = useState();
+  const [forecast, setForecast] = useState();
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://api.weatherapi.com/v1/current.json",
+      params: {
+        key: "739e4e0ba0334aa6b05185804232208",
+        q: "Baguio City",
+      },
+    }).then((res) => {
+      setWeatherData(res.data.current);
+    });
+  }, []);
+
+  return (
+    <>
+      <View>
+        <Stack.Screen options={{ title: "Home" }} />
+        <View>
+          <View className="flex flex-row flex-wrap gap-5 justify-center my-1">
+            <Text className="text-[16px] border basis-2/5 p-4 rounded-md">
+              Temperature: {weatherData ? weatherData.feelslike_c : "N/A"} C
+            </Text>
+            <Text className="text-[16px] border basis-2/5 p-4 rounded-md">
+              Humidity: {weatherData ? weatherData.humidity : "N/A"} %
+            </Text>
+            <Text className="text-[16px] border basis-2/5 p-4 rounded-md">
+              Windspeed: {weatherData ? weatherData.wind_kph : "N/A"} kph
+            </Text>
+            <Text className="text-[16px] border basis-2/5 p-4 rounded-md">
+              Chance of Rain: {weatherData ? weatherData.wind_kph : "N/A"} %
+            </Text>
+          </View>
+          <Link
+            href="/TakeAPicture"
+            className="text-[#FFF] mx-auto rounded-md bg-[#000] px-4 py-[10px] mb-2 mt-5"
+          >
+            Take a Picture
+          </Link>
+
+          <Link
+            href="/Upload"
+            className="text-[#FFF] mx-auto rounded-md bg-[#000] px-10 py-[10px]"
+          >
+            Upload
+          </Link>
+        </View>
+      </View>
+    </>
+  );
 };
 
 export default Home;
+
+// const styles = StyleSheet.create({
+//   button: {
+//     alignItems: "center",
+//     justifyContent: "center",
+//     paddingVertical: 12,
+//     paddingHorizontal: 32,
+//     borderRadius: 4,
+//     elevation: 3,
+//     backgroundColor: "black",
+//   },
+//   text: {
+//     fontSize: 16,
+//     lineHeight: 21,
+//     fontWeight: "bold",
+//     letterSpacing: 0.25,
+//     color: "white",
+//   },
+// });
--- app/TakeAPicture.jsx
+++ app/TakeAPicture.jsx
@@ -1,64 +1,65 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Camera } from "expo-camera";
 import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect, useFocusEffect, useRouter } from "expo-router";
 
 const TakeAPicture = () => {
-	const [hasPermission, setHasPermission] = useState(null);
-	const [type, setType] = useState(Camera.Constants.Type.back);
-	const ref = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const ref = useRef(null);
+  const router = useRouter();
 
-	useEffect(() => {
-		(async () => {
-			const { status } = await Camera.requestCameraPermissionsAsync();
-			setHasPermission(status === "granted");
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-	if (hasPermission === null) {
-		return <View />;
-	}
-	if (hasPermission === false) {
-		return <Text>No access to camera</Text>;
-	}
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
-	_takePhoto = async () => {
-		const photo = await ref.current.takePictureAsync();
-		console.debug(photo);
-	};
+  _takePhoto = async () => {
+    const photo = await ref.current.takePictureAsync();
+    if (photo) {
+      console.debug(photo);
+      router.push("/Preview?uri=" + photo.uri);
+    }
+  };
 
-	return (
-		<View className="flex flex-1">
-			<Camera
-				className="flex flex-1"
-				type={type}
-				ref={ref}
-			>
-				<View className="flex flex-1 bg-transparent flex-row">
-					{/* take */}
-					<TouchableOpacity
-						className="w-[80px] h-[80px] bg-tertiary items-center absolute bottom-[10%] left-[40.5%] rounded-full"
-						onPress={_takePhoto}
-					>
-						<Text className="mb-2.5 text-[#000] text-[18]"> </Text>
-					</TouchableOpacity>
+  return (
+    <View className="flex flex-1">
+      <Camera className="flex flex-1" type={type} ref={ref}>
+        <View className="flex flex-1 bg-transparent flex-row">
+          {/* take */}
+          <TouchableOpacity
+            className="w-[80px] h-[80px] bg-tertiary items-center absolute bottom-[10%] left-[40.5%] rounded-full"
+            onPress={_takePhoto}
+          >
+            <Text className="mb-2.5 text-[#000] text-[18]"> </Text>
+          </TouchableOpacity>
 
-					{/* flip */}
-					<TouchableOpacity
-						className="self-end items-center bg-tertiary"
-						onPress={() => {
-							setType(
-								type === Camera.Constants.Type.back
-									? Camera.Constants.Type.front
-									: Camera.Constants.Type.back
-							);
-						}}
-					>
-						<Text className="mb-2.5 text-accent text-[18]"> Flip </Text>
-					</TouchableOpacity>
-				</View>
-			</Camera>
-		</View>
-	);
+          {/* flip */}
+          <TouchableOpacity
+            className="self-end items-center bg-tertiary"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text className="mb-2.5 text-accent text-[18]"> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
 };
 
 export default TakeAPicture;
