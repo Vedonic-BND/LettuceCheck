9709c581ebfb5637880e98d5c2de41ebfbaa720f
Apple Juice <mallari23adrian@gmail.com>
2023-08-30 01:34:03 +0800

    added placeholder splash screen, icon on navbar, loading gif (temp)

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,46 +1,58 @@
 import { Stack, Tabs } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const TabArr = [
-	{
-		route: "/(tabs)/About",
-		label: "About",
-		bgColor: "#798777",
-		headTint: "#F8EDE3",
-	},
-	{
-		route: "/(tabs)/Home",
-		label: "Home",
-		bgColor: "#798777",
-		headTint: "#F8EDE3",
-	},
-	{
-		route: "/(tabs)/History",
-		label: "History",
-		bgColor: "#798777",
-		headTint: "#F8EDE3",
-	},
+  {
+    route: "/(tabs)/About",
+    label: "About",
+    bgColor: "#798777",
+    headTint: "#F8EDE3",
+    icon: "information",
+  },
+  {
+    route: "/(tabs)/Home",
+    label: "Home",
+    bgColor: "#798777",
+    headTint: "#F8EDE3",
+    icon: "home",
+  },
+  {
+    route: "/(tabs)/History",
+    label: "History",
+    bgColor: "#798777",
+    headTint: "#F8EDE3",
+    icon: "history",
+  },
 ];
 
+function makeIconRender(name) {
+  return ({ color, size }) => (
+    <MaterialCommunityIcons name={name} color={color} size={size} />
+  );
+}
+
 export default () => {
-	return (
-		<>
-			<Tabs>
-				{TabArr.map((item, index) => {
-					return (
-						<Tabs.Screen
-							key={index}
-							name={item.label}
-							options={{
-								// This tab will no longer show up in the tab bar.
-								href: item.route,
-								headerStyle: { backgroundColor: item.bgColor },
-								headerTintColor: item.headTint,
-								// tabBarShowLabel: false,
-							}}
-						/>
-					);
-				})}
-			</Tabs>
-		</>
-	);
+  return (
+    <>
+      <Tabs>
+        {TabArr.map((item, index) => {
+          return (
+            <Tabs.Screen
+              key={index}
+              name={item.label}
+              options={{
+                // This tab will no longer show up in the tab bar.
+                href: item.route,
+                headerStyle: { backgroundColor: item.bgColor },
+                headerTintColor: item.headTint,
+                tabBarIcon: makeIconRender(item.icon),
+                tabBarActiveTintColor: "#52694f",
+                // tabBarShowLabel: false,
+              }}
+            />
+          );
+        })}
+      </Tabs>
+    </>
+  );
 };
--- app/Diagnosis.jsx
+++ app/Diagnosis.jsx
@@ -4,32 +4,40 @@ import { useLocalSearchParams } from "expo-router";
 import { Image, Text, View } from "react-native";
 
 const Diagnosis = () => {
-	const image = useLocalSearchParams();
-	const { prediction } = usePredict(image.uri);
+  const image = useLocalSearchParams();
+  const { prediction } = usePredict(image.uri);
 
-	return (
-		<View>
-			<Text>Diagnosis</Text>
-			{image && (
-				<Image
-					source={{ uri: image.uri }}
-					style={{ width: 200, height: 200, marginVertical: 20 }}
-				/>
-			)}
-			<View style={{ marginTop: 20 }}>
-				{/* Labels will be populated here */}
-				{prediction ? (
-					prediction[0] > prediction[1] ? (
-						<Text>Not Healthy</Text>
-					) : (
-						<Text>Healthy</Text>
-					)
-				) : (
-					<Text>Loading...</Text>
-				)}
-			</View>
-		</View>
-	);
+  return (
+    <View>
+      {image && (
+        <Image
+          className="mx-auto rounded-md"
+          source={{ uri: image.uri }}
+          style={{ width: 200, height: 200, marginVertical: 20 }}
+        />
+      )}
+      <View style={{ marginTop: 20 }}>
+        {/* Labels will be populated here */}
+        {prediction ? (
+          prediction[0] > prediction[1] ? (
+            <Text className="mx-auto">Not Healthy</Text>
+          ) : (
+            <Text className="mx-auto">Healthy</Text>
+          )
+        ) : (
+          <Image
+            className="mx-auto"
+            source={require("../assets/loading/load.gif")}
+            style={{
+              width: 200,
+              height: 200,
+              marginVertical: 20,
+            }}
+          />
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default Diagnosis;
