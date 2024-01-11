b219099ab1f2d8f333c165e4672fbcd0120ddd41
Apple Juice <mallari23adrian@gmail.com>
2024-01-11 23:15:38 +0800

    Update: Added persistent data
    Added react asynch storage for storing history data

--- app/(tabs)/History.jsx
+++ app/(tabs)/History.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const History = () => {
+	const [history, setHistory] = useState(null)
+
+	function refresh() {
+		AsyncStorage.getAllKeys().then((res) => {
+			AsyncStorage.multiGet(res).then((res) => {
+				setHistory(res)
+				res.map((item, index) => {
+					item[1] = JSON.parse(item[1])
+					setHistory(history => [...history, item])
+				})
+			})
+		})
+	}
+	useEffect(() => {
+		refresh()
+	},[])
 	return (
 		<>
 			<Stack.Screen options={{ title: "History" }} />
-			<View>
-				<Text className="text-red-700 text-lg">history</Text>
-			</View>
+			<ScrollView className="m-2">
+				{history && history.map((item, index) => {
+					console.log(item[1].accuracy)
+					return (
+						<View className="border p-2.5 rounded-md flex flex-row justify-around self-center w-full mb-2">
+							<Text className="text-red-700 text-lg h-7 my-auto">{item[1].date}</Text>
+							<Text className="text-red-700 text-lg h-7 my-auto">{parseFloat(item[1].accuracy *100).toFixed(2)}%</Text>
+							<View className="rounded-md w-16 h-16 my-auto">
+								<Image
+								className="mx-auto rounded-md"
+								source={{ uri: item[1].image }}
+								style={{ width: 64, height: 64 }}
+								/>
+							</View>
+						</View>
+					)
+				})}
+			</ScrollView>
 		</>
 	);
 };
--- app/Diagnosis.jsx
+++ app/Diagnosis.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import usePredict from "../constants/usePredict";
 import { useLocalSearchParams } from "expo-router";
 import { Image, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 const Diagnosis = () => {
   const image = useLocalSearchParams();
   const { prediction } = usePredict(image.uri);
-
+  const transaction = prediction ? {
+    image: image.uri,
+    prediction: prediction[0] > prediction[1] ? "Not Healthy" : "Healthy",
+    accuracy: prediction[0] > prediction[1] ? prediction[0] : prediction[1],
+    date: new Date().toLocaleDateString(),
+  } : ""
+  const today = new Date(Date.now())
+  AsyncStorage.setItem(`transaction-${today.toISOString()}`, JSON.stringify(transaction)).then(() => {
+    console.log("saved")
+  });
   return (
-    <View>
+    <View className="mt-48">
       {image && (
         <Image
           className="mx-auto rounded-md"
@@ -20,9 +31,9 @@ const Diagnosis = () => {
         {/* Labels will be populated here */}
         {prediction ? (
           prediction[0] > prediction[1] ? (
-            <Text className="mx-auto">Not Healthy</Text>
+            <Text className="mx-auto font-bold text-2xl">Not Healthy</Text>
           ) : (
-            <Text className="mx-auto">Healthy</Text>
+            <Text className="mx-auto font-bold text-2xl">Healthy</Text>
           )
         ) : (
           <Image
