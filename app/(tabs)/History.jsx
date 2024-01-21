98589f7b701860671dab15843682548fde3a62f6
Apple Juice <mallari23adrian@gmail.com>
2024-01-21 15:32:01 +0800

    Update: Persistent Storage and Modal
    Added persistent storage using Expo-SQLite and Created a model for viewing images in history

--- app/(tabs)/History.jsx
+++ app/(tabs)/History.jsx
@@ -1,16 +1,138 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  RefreshControl,
+  SafeAreaView,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+} from "react-native";
+import db from "../../constants/database.js";
+import useDate from "../../constants/useDate.js";
 
 const History = () => {
-	return (
-		<>
-			<Stack.Screen options={{ title: "History" }} />
-			<View>
-				<Text className="text-red-700 text-lg">history</Text>
-			</View>
-		</>
-	);
+  const [history, setHistory] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [showImage, setShowImage] = useState(null);
+  useEffect(() => {
+    onRefresh();
+  }, []);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const showSelected = (item) => {
+    setShowDetails(true);
+    setShowImage(item.image);
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM history",
+        [],
+        (_, resultSet) => {
+          // Handle the result set here
+          const { rows } = resultSet;
+          setHistory(rows._array);
+          setRefreshing(false);
+        },
+        (_, error) => {
+          console.error("Error fetching history:", error);
+        }
+      );
+    });
+  }, []);
+  return (
+    <>
+      <SafeAreaView>
+        <Stack.Screen options={{ title: "History" }} />
+        <ScrollView
+          className="m-2"
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <View className="p-2.5 rounded-md flex flex-row justify-around self-center w-full mb-2">
+            <Text className="text-red-700 text-lg h-7 my-auto font-semibold">
+              Datetime
+            </Text>
+            <Text className="text-red-700 text-lg h-7 my-auto font-semibold">
+              Accuracy
+            </Text>
+            <Text className="text-red-700 text-lg h-7 my-auto font-semibold">
+              Prediction
+            </Text>
+            <Text className="text-red-700 text-lg h-7 my-auto font-semibold">
+              Image
+            </Text>
+          </View>
+          {history.length > 0 ? (
+            history.map((item, index) => {
+              return (
+                <View
+                  className="border p-2.5 rounded-md flex flex-row justify-around self-center w-full mb-2"
+                  key={index}
+                >
+                  <View className="flex my-auto">
+                    <Text className="text-red-700 text-sm text-center">
+                      {useDate(item.date).split(",")[0]}
+                    </Text>
+                    <Text className="text-red-700 text-sm text-center">
+                      {useDate(item.date).split(",")[1]}
+                    </Text>
+                  </View>
+                  <Text className="text-red-700 text-lg h-7 my-auto">
+                    {parseFloat(item.accuracy * 100).toFixed(2)}%
+                  </Text>
+                  <Text className="text-red-700 text-lg h-7 my-auto">
+                    {item.prediction}
+                  </Text>
+                  <TouchableOpacity onPress={() => showSelected(item)}>
+                    <View className="rounded-md w-16 h-16 my-auto">
+                      <Image
+                        className="mx-auto rounded-md"
+                        source={{ uri: item.image }}
+                        style={{ width: 64, height: 64 }}
+                      />
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              );
+            })
+          ) : (
+            <View className="mx-auto">
+              <Text className="text-2xl">No History</Text>
+            </View>
+          )}
+        </ScrollView>
+        <Modal animationType="slide" transparent={true} visible={showDetails}>
+          <View className="h-3/4 w-full bg-[#25292e] rounded-t-2xl absolute bottom-0">
+            <View className="h-9 bg-[#464C55] rounded-t-xl px-5 items-center justify-between flex-row-reverse">
+              <Pressable
+                onPress={() => {
+                  setShowDetails(false);
+                }}
+              >
+                <Text className="text-[#FFFFFF] font-semibold">Close</Text>
+              </Pressable>
+            </View>
+            <View className="h-full flex justify-center items-center rounded-b-xl px-8 pb-8">
+              <Image
+                className="rounded-md h-5/6 w-full"
+                source={{ uri: showImage }}
+              />
+            </View>
+          </View>
+        </Modal>
+      </SafeAreaView>
+    </>
+  );
 };
 
 export default History;
--- app/Diagnosis.jsx
+++ app/Diagnosis.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import usePredict from "../constants/usePredict";
 import { useLocalSearchParams } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, ActivityIndicator } from "react-native";
+import db from "../constants/database.js";
 
 const Diagnosis = () => {
   const image = useLocalSearchParams();
-  const { prediction } = usePredict(image.uri);
+  const [prediction, setPrediction] = useState([]);
+  const fetchPrediction = usePredict(image.uri);
+
+  useEffect(() => {
+    if (fetchPrediction !== null) {
+      setPrediction(fetchPrediction);
+      db.transaction((tx) => {
+        tx.executeSql(
+          "INSERT INTO history (date, image, accuracy, prediction) VALUES (?, ?, ?, ?);",
+          [
+            Date.now(),
+            image.uri,
+            fetchPrediction[0] > fetchPrediction[1]
+              ? fetchPrediction[0]
+              : fetchPrediction[1],
+            prediction[0] > prediction[1] ? "Not Healthy" : "Healthy",
+          ],
+          (_, resultSet) => {
+            console.log("Item inserted:", resultSet);
+          },
+          (_, error) => {
+            console.error("Error inserting item:", error);
+          }
+        );
+      });
+    }
+  }, [image.uri, fetchPrediction]);
 
   return (
-    <View>
+    <View className="mt-48">
       {image && (
         <Image
           className="mx-auto rounded-md"
@@ -17,23 +44,16 @@ const Diagnosis = () => {
         />
       )}
       <View style={{ marginTop: 20 }}>
-        {/* Labels will be populated here */}
-        {prediction ? (
+        {prediction.length > 0 ? (
           prediction[0] > prediction[1] ? (
-            <Text className="mx-auto">Not Healthy</Text>
+            <Text className="mx-auto font-semibold text-lg">Not Healthy</Text>
           ) : (
-            <Text className="mx-auto">Healthy</Text>
+            <Text className="mx-auto font-semibold text-lg">Healthy</Text>
           )
         ) : (
-          <Image
-            className="mx-auto"
-            source={require("../assets/loading/load.gif")}
-            style={{
-              width: 200,
-              height: 200,
-              marginVertical: 20,
-            }}
-          />
+          <Text className="mx-auto">
+            <ActivityIndicator size="large" color="#0073e6" />
+          </Text>
         )}
       </View>
     </View>
